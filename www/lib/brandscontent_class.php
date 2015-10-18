3d030173b0b4d659eba96fbc8f15142b2fb26ca7
Михаил <felix9988@yandex.ru>
2015-10-18 17:51:23 +0300

    brands_html

---
+++ www/lib/brandscontent_class.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "modules_class.php";
+
+class BrandsContent extends Modules {    
+
+    public function __construct($db) {
+        parent::__construct($db);
+        
+    }
+
+    protected function getTitle() {        
+            return "Список производителей";
+    }
+
+    protected function getDescription() {
+        return "Список производителей";
+    }
+
+    protected function getKeyWords() {
+        return "russian markt, производители, список производителей";
+    }
+
+    protected function getTop() {
+        return $this->getTemplate("brandlist_content");
+    }
+
+}
+?>
